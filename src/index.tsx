f750c15b701d82a0b7f2632a1ccd52d817ee1a1f
Rodrigo Bergamin <rodrigobergamindev@gmail.com>
2022-04-20 14:39:07 -0300

    .

--- src/index.tsx
+++ src/index.tsx
@@ -6,9 +6,7 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
 
